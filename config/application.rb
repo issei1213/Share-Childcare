f05dc6c139c8fda02931e50f0396bf8d54d7fade
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-03-24 17:16:52 +0900

    create model validation

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -15,5 +14,8 @@ module ShareChildcare
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
+
   end
 end
