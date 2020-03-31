125657f20b60772eb7f3abd792316866fb9bcf84
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-03-31 14:26:06 +0900

    completed login function

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,24 +1,19 @@
 module DeviseHelper
-  def devise_error_messages!
+  def bootstrap_devise_error_messages!
     return "" if resource.errors.empty?
 
     html = ""
-    messages = resource.errors.full_messages.each do |errmsg|
+    resource.errors.full_messages.each do |error_message|
       html += <<-EOF
       <div class="alert alert-danger alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert">
           <span aria-hidden="true">&times;</span>
           <span class="sr-only">close</span>
         </button>
-        #{errmsg}
+        #{error_message}
       </div>
       EOF
     end
     html.html_safe
   end
-
-  def devise_error_messages?
-    resource.errors.empty? ? false : true
-  end
-
 end
