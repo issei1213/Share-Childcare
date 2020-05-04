a96611c335695023c4dd0bf6a2c391a8b415c270
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-05-04 14:53:11 +0900

    installed gem annotate

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Order < ApplicationRecord
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
