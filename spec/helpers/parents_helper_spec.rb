e16d88bac166b94f4a84505a5818bdd4aafcefac
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-05-12 21:49:03 +0900

    updates rspec file

--- spec/helpers/parents_helper_spec.rb
+++ spec/helpers/parents_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe UserParentsHelper, type: :helper do
+RSpec.describe ParentsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
