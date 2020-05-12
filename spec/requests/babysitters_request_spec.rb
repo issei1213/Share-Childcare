047d4431000296579f07c447cf73c4ae8311d295
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-05-12 21:53:14 +0900

    updated rspec file

---
+++ spec/requests/babysitters_request_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+
+# RSpec.describe "Babysitters", type: :request do
+
+#   describe "GET /show" do
+#     it "returns http success" do
+#       get "/babysitters/show"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+# end
