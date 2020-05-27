71e3ff4d4dcce97d8e34a5b22b90630c601eed94
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-05-27 18:04:57 +0900

    update bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require activestorage
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
-//= require bootstrap.bundle.min
+//= require bootstrap-material-design.min
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
