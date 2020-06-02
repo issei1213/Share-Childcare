2fe8abbe94a3a78b26c327915930baf024d250df
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-02 19:58:37 +0900

    Update baby_infomation_spec.rb

--- spec/models/baby_infomation_spec.rb
+++ spec/models/baby_infomation_spec.rb
@@ -28,4 +28,24 @@ RSpec.describe BabyInfomation, type: :model do
       expect(baby_infomation).to be_valid
     end
   end
+
+  context "can not save" do
+    it "性別が空では登録ができない事" do
+      baby_infomation = build(:baby_infomation, sex: "")
+      baby_infomation.valid?
+      expect(baby_infomation.errors[:sex]).to include("を入力してください")
+    end
+
+    it "名前が空では登録ができない事" do
+      baby_infomation = build(:baby_infomation, name: "")
+      baby_infomation.valid?
+      expect(baby_infomation.errors[:name]).to include("を入力してください")
+    end
+
+    it "年齢が空では登録ができない事" do
+      baby_infomation = build(:baby_infomation, age: "")
+      baby_infomation.valid?
+      expect(baby_infomation.errors[:age]).to include("を入力してください")
+    end
+  end
 end
