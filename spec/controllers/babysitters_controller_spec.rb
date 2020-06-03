ab9af2a6410e1ca0722f2eb6b97003f29d4634a9
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-03 19:53:32 +0900

    Update babysitters_controller_spec.rb

--- spec/controllers/babysitters_controller_spec.rb
+++ spec/controllers/babysitters_controller_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe BabysittersController, type: :controller do
       before do
         login user
         get :edit, params: { id: babysitter }
-        babysitter.babysitter_images
+        # babysitter.babysitter_images
       end
       it "HTTPステータスが200であることを確認" do
         expect(response.status).to eq(200)
