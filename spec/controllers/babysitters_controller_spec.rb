9220454b9bd0a4957d7e9e928e4259801c363e72
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-04 18:03:57 +0900

    Update babysitters_controller_spec.rb

--- spec/controllers/babysitters_controller_spec.rb
+++ spec/controllers/babysitters_controller_spec.rb
@@ -54,7 +54,6 @@ RSpec.describe BabysittersController, type: :controller do
         expect(response).to render_template :edit
       end
       it "インスタンス変数を確認する" do
-        binding.pry
         expect(assigns(:babysitter)).to eq babysitter
       end
     end
