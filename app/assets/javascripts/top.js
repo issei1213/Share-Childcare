d01018a7799f6787ceadde594acebebc466b0799
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-06 16:21:07 +0900

    updated jquery slider

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,14 +1,20 @@
-$(function() {
-  $(document).ready(function() {
-  // イメージのスライド
-    $(".top-image-box").slick({
-      autoplay: true,
-      autoplaySpeed: 5000,
-      fade: true
-    });
-  });
-  // イメージボタンを隠す
-  $(document).ready(function() {
-    $(".slick-arrow").hide();
+// $(function() {
+//   $(document).ready(function() {
+//   // イメージのスライド
+//     $(".top-image-box").slick({
+//       autoplay: true,
+//       autoplaySpeed: 5000,
+//       fade: true
+//     });
+//   });
+//   // イメージボタンを隠す
+//   $(document).ready(function() {
+//     $(".slick-arrow").hide();
+//   });
+// });
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    autoplay:true,  //←自動再生
+    dots:true      //←ドットの描写
   });
 });
