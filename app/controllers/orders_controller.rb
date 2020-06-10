5aab3c26c00a2c0080981d6193e856a6d317773e
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-10 21:41:25 +0900

    completed view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,5 +9,17 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order_params
+      redirect_to root_path
+    else
+      flash[:error_messages] = @order.errors.full_messages
+      render :new
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:)
   end
 end
