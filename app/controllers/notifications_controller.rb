0eee5e98f6c69a90fefc923187afbee5c55494e3
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-06-22 22:40:42 +0900

    updated view

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,6 @@
 class NotificationsController < ApplicationController
   def index
+    binding.pry
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
