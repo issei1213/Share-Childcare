6795727ee703debef1c6ab9af40b68931bb855c9
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-09 06:34:19 +0900

    update

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -3,4 +3,9 @@ $(function(){
     autoplay:true,  //←自動再生
     dots:true      //←ドットの描写
   });
+  // プレビューボタンの削除
+  $(".slick-prev.slick-arrow").remove();
+  // 画像下のボタン削除
+  $(".slick-next.slick-arrow").remove();
+  $(".slick-dots").remove();
 });
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,8 @@ class User < ApplicationRecord
   has_one :babysitter
   has_one :parent
   has_many :orders
-  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
-  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+  has_many :active_notifications, class_name: "Notification", foreign_key: "visitor_id", dependent: :destroy
+  has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
 
   devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
 
