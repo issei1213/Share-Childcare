f4c0fd94059d94c4ff6235d2ca205debfa91e03d
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-11 17:51:36 +0900

    completed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :order_set_find, only: [:show, :edit, :update, :destroy, :approve, :cancel]
+
 
   def root
     @user = User.new
@@ -20,33 +22,55 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
     @order.update_notification_checked!(@order.notifications)
   end
 
   def edit
-    @order = Order.find(params[:id])
     @babysitter = Babysitter.find(params[:babysitter_id])
     @parent = Parent.find(params[:parent_id])
   end
 
   def update
-    @order = Order.find(params[:id])
     @order.update(order_params)
     @order.notification_order!("ordered-updated", current_user)
     redirect_to notifications_path, notice: "変更しました。ベビーシッターから連絡があるまで暫くお待ちください。"
   end
 
   def destroy
-    @order = Order.find(params[:id])
     @order.destroy
     redirect_to notifications_path, notice: "削除しました。"
+  end
+
+  def approve
+    if @order.status == "ordered"
+      @order.update(status: "approved")
+      @order.notification_order!("ordered-approved", current_user)
+      redirect_to notifications_path, notice: "依頼を承諾しました。チャットルームを開放します。"
+    else
+      flash[:error] = "この依頼は回答済みです。"
+      redirect_to notifications_path
+    end
+  end
+
+  def cancel
+    if @order.status == "ordered"
+      @order.update(status: "canceled")
+      @order.notification_order!("ordered-canceled", current_user)
+      redirect_to notifications_path, notice: "依頼をキャンセルしました。"
+    else
+      flash[:error] = "この依頼は回答済みです。"
+      redirect_to notifications_path
+    end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:year, :month, :date, :hour_top, :hour_down, :money_hour, :money_option, :memo, :babysitter_id, :page, :parent_id).merge(user_id: current_user.id)
+    params.require(:order).permit(:year, :month, :date, :hour_top, :hour_down, :money_hour, :money_option, :memo, :page, :babysitter_id, :parent_id).merge(user_id: current_user.id)
+  end
+
+  def order_set_find
+    @order = Order.find(params[:id])
   end
 
 end
