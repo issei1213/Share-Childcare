6732b635ec58bcff0ed09e91648a350b4247908b
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-23 16:47:03 +0900

    updated view

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,8 @@
 class HomesController < ApplicationController
   def index
     if user_signed_in? && current_user.babysitter.present?
-      @babysitters = Babysitter.where.not(id: current_user.babysitter.id).page(params[:page]).per(8)
+      @q = Babysitter.where.not(id: current_user.babysitter.id).ransack(params[:q])
+      @babysitters = @q.result(distinct: true).page(params[:page]).per(8)
     else
       @babysitters = Babysitter.all.page(params[:page]).per(8)
     end
