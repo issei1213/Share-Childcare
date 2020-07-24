a53a091174955dc1e63e45903068ca1054a9b335
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-24 17:30:03 +0900

    Update notifications_controller.rb

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.includes(:visited).page(params[:page]).per(15)
+    @notifications = current_user.passive_notifications.includes(:visitor).page(params[:page]).per(15)
     # @notifications = Notification.where(["visitor_id = ? or visited_id = ?", current_user.id, current_user.id]).page(params[:page]).per(15)
   end
 end
