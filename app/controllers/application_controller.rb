8ea737b193a847250635a8fd94cd49a0a133577c
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-25 19:20:17 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :store_user_location!, if: :storable_location?
+  # before_action :store_user_location!, if: :storable_location?
   # before_action :authenticate_user!
 
-  private
+  # private
 
-  def storable_location?
-    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
-  end
+  # def storable_location?
+  #   request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  # end
 
-  def store_user_location!
-    store_location_for(:user, request.fullpath)
-  end
+  # def store_user_location!
+  #   store_location_for(:user, request.fullpath)
+  # end
 
-  # ログアウトした時もフレンドリーフォワーディング
-  def after_sign_out_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || super
-  end
+  # # ログアウトした時もフレンドリーフォワーディング
+  # def after_sign_out_path_for(resource_or_scope)
+  #   stored_location_for(resource_or_scope) || super
+  # end
 end
