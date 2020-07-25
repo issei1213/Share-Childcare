7b705c84734dd5e75dfff5be084c23d4d2ada255
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-07-25 13:48:36 +0900

    completed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,23 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  # ログイン失敗後は def failed に飛ぶように変更
+  def create
+    auth_options = { scope: resource_name, recall: "#{controller_path}#failed" }
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+  # ログイン失敗の時は直前のURLにリダイレクトする
+  def failed
+    flash[:error] = "メールアドレスまたはパスワードが違います。"
+    redirect_to params[:user][:url]
+  end
+
+
   # GET /resource/sign_in
   # def new
   #   super
